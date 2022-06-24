f905880a473a90d680db2667f82ef5db9b4bee2f
Maxim Drunin <123@gmail.com>
2022-06-24 13:46:48 +0300

    Stage4 done

--- script/s4/gochroot.sh
+++ script/s4/gochroot.sh
@@ -5,7 +5,7 @@ cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos
 
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
 
-cp /gentooConfig /mnt/gentoo/
+cp -r /gentooConfig-main /mnt/gentoo/
 
 mount --types proc /proc /mnt/gentoo/proc
 mount --rbind /sys /mnt/gentoo/sys
--- script/s4/postchroot.sh
+++ script/s4/postchroot.sh
@@ -18,3 +18,10 @@ echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags
 
 echo "America/New_York" > /etc/timezone
 emerge --config sys-libs/timezone-data
+
+echo "en_US ISO-8859-1" >> /etc/locale.gen
+echo "en_US.UTF-88 UTF-8" >> /etc/locale.gen
+locale-gen
+eselect locale set 6
+
+env-update && source /etc/profile && export PS1="(chroot) ${PS1}"
